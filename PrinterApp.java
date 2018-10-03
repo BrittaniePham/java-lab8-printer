4ae026b3370cc65471f81385be95b320b2d109af
Brittanie Pham <brittaniepham@gmail.com>
2018-10-02 19:07:19 -0600

    PrinterApp Class

--- PrinterApp.java
+++ PrinterApp.java
@@ -4,7 +4,18 @@ package lab8_printer;
  */
 
 public abstract class PrinterApp {
-
+	
+	private int serialNumber;
+	
+	public void Printer(int number) {
+		serialNumber = number;
+	}
+	
+	public abstract void print();
+	
+	@Override
+	public String toString() {
+		return getClass().getSimpleName() + " #" + serialNumber;
+//		return super.toString();
+	}
 }
-
-
