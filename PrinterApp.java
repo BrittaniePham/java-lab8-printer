83cb884eb2cc5a239901952dd2a28b7b2b9d0335
Brittanie Pham <brittaniepham@gmail.com>
2018-10-03 10:47:44 -0600

    PrinterApp objectives

--- PrinterApp.java
+++ PrinterApp.java
@@ -1,5 +1,7 @@
 package lab8_printer;
 
+import java.util.ArrayList;
+
 public class PrinterApp {
 
 	public static void main(String[] args) {
@@ -9,9 +11,20 @@ public class PrinterApp {
 		
 		LaserPrinter laser = new LaserPrinter(2345678);
 		System.out.println(laser);
-		System.out.println("Remaining Toner: " + laser.getRemainingToner() + "%");
+		System.out.println("Remaining Toner: " + laser.getRemainingToner() + "%\n");
 		
 		System.out.println("List of Printers: ");
+		ArrayList<Printer> printers = new ArrayList<>();
+		printers.add(ink);
+		printers.add(laser);
+		
+		for(Printer p : printers) {
+			System.out.println(p);
+			for(int i = 0; i < 11; i++) {
+				p.print();
+			}
+			System.out.println();
+		}
 	}
 
 }
